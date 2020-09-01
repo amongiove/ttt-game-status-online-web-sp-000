351a08f5eb4abb0e9a7f61a3b65291ab4b06edf8
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-01 20:09:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,10 +23,6 @@ def won?(board)
         return win_combination
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination
-      elsif position_1 == " " && position_2 == " " && position_3 == " "
-        return false
-      elsif position_1 == nil && position_2 == nil && position_3 == nil
-        return false
       else
         false
       end
