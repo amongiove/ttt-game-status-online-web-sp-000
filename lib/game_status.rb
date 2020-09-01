6bd6deef85339ee746d8df3ba2f7387490c9f662
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-01 19:16:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,11 @@
-# Helper Method
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]
+]
+
+    
