4ffa7559fefc28e979cddf1797071c1118535801
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-01 19:45:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ def won(board)
 
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
+    position_3 = board[win_index_3]
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
@@ -27,3 +27,4 @@ def won(board)
         false
       end
     end
+  end
