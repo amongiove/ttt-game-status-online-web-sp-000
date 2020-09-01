fa32a2f0cd20538e646c385ceef572b1f96f82d9
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-01 20:16:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,5 +30,5 @@ def won?(board)
   end
 
 def full?(board)
-  board.any?{|index| index != nil || index != " " || index != ""}
+  board.all?{|index| index != nil || index != " " || index != ""}
 end
