55d1b3ca9b25b21753243737b1fcc27bc0f557bb
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-01 20:55:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,10 +45,12 @@ def full?(board)
 end
 
 def draw?(board)
-  if won?(board) == true
+  if won?(board) == true 
     return false
-  else won?(board) == false && full?(board) == true
+  elsif won?(board) == false && full?(board) == true
     return true
+  else
+   return false
   end
 end
 
